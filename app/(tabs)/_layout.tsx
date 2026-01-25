8c11ad22375e9c027b59dc57bcf716535638096e
pappu9456 <pappu@myvisionlabs.ai>
2026-01-25 18:49:59 +0530

    feat: Add tabBarIcon options for index, explore, and settings screens in TabLayout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,7 @@
+
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 export default function TabLayout() {
@@ -13,18 +15,25 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Reddit',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Playgrounds',
+          title: 'Playgrounds', tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="play-arrow" color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: 'Settings',
+          title: 'Settings', tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="settings" color={color} size={size} />
+          ),
         }}
       />
     </Tabs>
